fc02e4d38c37ae9fbdb22fa51818680c81be6774
dd64 <87917282+dd6478@users.noreply.github.com>
2024-03-19 15:30:51 +0100

    Adding onOptionsItemSelected

--- app/src/main/java/fr/eseo/monmenu/MainActivity.kt
+++ app/src/main/java/fr/eseo/monmenu/MainActivity.kt
@@ -3,10 +3,9 @@ package fr.eseo.monmenu
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
-import androidx.activity.enableEdgeToEdge
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,5 +18,23 @@ class MainActivity : AppCompatActivity() {
         val mainMenuInflater : MenuInflater = menuInflater
         mainMenuInflater.inflate(R.menu.main_menu, menu)
         return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.bed -> {
+                Toast.makeText(this, "Bed", Toast.LENGTH_SHORT).show()
+                return true
+            }
+            R.id.bathtub -> {
+                Toast.makeText(this, "Bath", Toast.LENGTH_SHORT).show()
+                return true
+            }
+            R.id.accessible -> {
+                Toast.makeText(this, "Accessible", Toast.LENGTH_SHORT).show()
+                return true
+            }
+    }
+        return super.onOptionsItemSelected(item)
     }
 }
